fd00c71f4bf463eaa9c35fd396c3396878e6d934
lukindv77 <lukindv@gmail.com>
2026-09-05 19:15:25 +0700

    feat: preflight custom binary size limits

--- src/Clipensk.Core/Clipboard/IClipboardCustomBinaryContentReader.cs
+++ src/Clipensk.Core/Clipboard/IClipboardCustomBinaryContentReader.cs
@@ -4,8 +4,9 @@ public interface IClipboardCustomBinaryContentReader
 {
     bool SupportsFormat(string formatName);
 
-    ValueTask<byte[]> ReadAsync(
+    ValueTask<byte[]?> ReadWithinLimitAsync(
         IClipboardContentSnapshot contentSnapshot,
         string formatName,
+        long? maxBytes,
         CancellationToken cancellationToken = default);
 }
