2f94434712ff57c2234e295b31a0c11aff26703d
lukindv77 <lukindv@gmail.com>
2026-09-05 22:36:41 +0700

    feat: model application identity alias lookup

---
+++ src/Clipensk.Core/Applications/ApplicationIdentityAliasLookup.cs
@@ -0,0 +1,5 @@
+namespace Clipensk.Core.Applications;
+
+public sealed record ApplicationIdentityAliasLookup(
+    ApplicationId? ApplicationUserModelIdApplicationId,
+    ApplicationId? ExecutablePathApplicationId);
