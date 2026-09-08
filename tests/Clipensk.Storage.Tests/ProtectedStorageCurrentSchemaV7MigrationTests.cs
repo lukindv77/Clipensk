cc06ec3572eb402d3f2ee310d1aadb685e3d16b5
lukindv77 <lukindv@gmail.com>
2026-09-08 23:09:26 +0700

    Preserve v7 migration test policy type

--- tests/Clipensk.Storage.Tests/ProtectedStorageCurrentSchemaV7MigrationTests.cs
+++ tests/Clipensk.Storage.Tests/ProtectedStorageCurrentSchemaV7MigrationTests.cs
@@ -15,7 +15,7 @@ public sealed class ProtectedStorageCurrentSchemaV7MigrationTests
             ClipboardCapturePolicyRule.Allow,
             new Dictionary<string, ClipboardFormatCapturePolicy>
             {
-                ["Text"] = new(ClipboardFormatCapturePolicyRule.Allow, 4096),
+                ["Text"] = new(ClipboardCapturePolicyRule.Allow, 4096),
             }));
         await environment.CustomBinaryConfigurations.InitializeAsync("Private.Format", ".dat");
         environment.DowngradeToV6();
